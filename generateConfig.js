251be17c777a5165e32911512235561291264250
Deep Bhatt <dbhatt@pointmotion.us>
2022-05-16 15:03:17 +0530

    ci: 🎡 run script to generate a config

--- generateConfig.js
+++ generateConfig.js
@@ -6,12 +6,12 @@ try {
 
   // An extremely simple config generator.
   // TODO: Build a better solution when there are more variables to replace.
-  doc.endpoint = process.env.HASURA_ENDPOINT
-  doc.admin_secret = process.env.HASURA_ADMIN_SECRET
+  doc.endpoint = process.env.HASURA_ENDPOINT;
+  doc.admin_secret = process.env.HASURA_ADMIN_SECRET;
 
   // saving file back as YAML
-  const config = yaml.dump(doc)
-  fs.writeFileSync('config.yaml', config)
+  const config = yaml.dump(doc);
+  fs.writeFileSync('config.yaml', config);
 
 } catch (e) {
   console.log(e);
