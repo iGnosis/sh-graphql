90d7859cf3c4f7aeb7c50e633b4cdae070e06833
Deep Bhatt <dbhatt@pointmotion.us>
2022-07-29 15:56:36 +0530

    fix: 🐛 add custom migration for game_name ENUMs

---
+++ migrations/default/1659090151432_insert_game_name_enums/up.sql
@@ -0,0 +1,4 @@
+-- game_name
+INSERT INTO "public"."game_name"("name") VALUES (E'sound_slicer');
+INSERT INTO "public"."game_name"("name") VALUES (E'beat_boxer');
+INSERT INTO "public"."game_name"("name") VALUES (E'sit_stand_achieve');
