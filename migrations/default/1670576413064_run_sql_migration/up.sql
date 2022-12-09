d892fd0cdf36dddb1d4dd006c40f1f3634cb62f9
vigneshrajj <vigneshhrajj@gmail.com>
2022-12-09 14:30:44 +0530

    fix: 🐛 mark enum as migration

---
+++ migrations/default/1670576413064_run_sql_migration/up.sql
@@ -0,0 +1,7 @@
+INSERT INTO subscription_status (status, description) VALUES ('active', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('cancelled', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('archived', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('payment_pending', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('blocked', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('trial_period', NULL);
+INSERT INTO subscription_status (status, description) VALUES ('trial_expired', NULL);
