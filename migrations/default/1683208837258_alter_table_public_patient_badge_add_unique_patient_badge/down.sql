7014740644db31b36280b9cec33b51f613312e0e
Deep Bhatt <dbhatt@pointmotion.us>
2023-05-04 20:59:29 +0530

    feat: 🎸 add API to update patient's user context

---
+++ migrations/default/1683208837258_alter_table_public_patient_badge_add_unique_patient_badge/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."patient_badge" drop constraint "patient_badge_patient_badge_key";
---
+++ migrations/default/1683208837258_alter_table_public_patient_badge_add_unique_patient_badge/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."patient_badge" add constraint "patient_badge_patient_badge_key" unique ("patient", "badge");
